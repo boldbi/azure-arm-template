4e6697ea007499db012cfc27c92040790a318efa
Vinoth-Krishnamoorthy <92359471+Vinoth-Krishnamoorthy@users.noreply.github.com>
2023-01-04 13:18:39 +0530

    Moving latest package files

--- bi/web/wwwroot/webdesignerservice/scripts/ej1.treemap.all.js
+++ bi/web/wwwroot/webdesignerservice/scripts/ej1.treemap.all.js
@@ -1,7 +1,7 @@
 /*!
 *  filename: ej1.treemap.all.js
-*  version : 5.3.80
-*  Copyright Syncfusion Inc. 2001 - 2022. All rights reserved.
+*  version : 5.3.83
+*  Copyright Syncfusion Inc. 2001 - 2023. All rights reserved.
 *  Use of this code is subject to the terms of our license.
 *  A copy of the current license can be obtained at any time by e-mailing
 *  licensing@syncfusion.com. Any infringement will be prosecuted under
