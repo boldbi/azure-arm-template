762cad87d04992d7b561d4a60eacfe558c088c1b
SivanesanSyncfusion <sivanesan.saravanan@syncfusion.com>
2023-07-19 07:01:06 +0000

    Latest packages update

--- bi/dataservice/entrypoint.sh
+++ bi/dataservice/entrypoint.sh
@@ -2,7 +2,7 @@
 
 bash install-optional.libs.sh
 
-if [ -d "/application/app_data/bi/dataservice/CustomWidgets" ]; then
+if [ ! -d "/application/app_data/bi/shapefiles" ] || [ -d "/application/app_data/bi/dataservice/CustomWidgets" ]; then
     if [ -f "/application/utilities/customwidgetupgrader/CustomWidgetUpgrader.dll" ]; then
 	    dotnet /application/utilities/customwidgetupgrader/CustomWidgetUpgrader.dll true
 	fi
