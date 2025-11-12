d050e2aacb370a86115570e629727bf72165a860
KingsleyAnbinnesan <kingsley.anbinnesan@syncfusion.com>
2025-11-12 08:40:49 +0000

    v14.1.20 Package update

--- bi/web/configuration/onpremise/mssql/versioned_scripts/latest.sql
+++ bi/web/configuration/onpremise/mssql/versioned_scripts/latest.sql
@@ -1,6 +1,6 @@
 ALTER TABLE [BOLDBI_Item] ADD [DashboardLogo] [nvarchar](1026) NULL;
 
-ALTER TABLE [BOLDBI_ScheduleDetail] ALTER COLUMN [AIInsightSummaryEnabled] [bit] NOT NULL DEFAULT 0;
+ALTER TABLE [BOLDBI_ScheduleDetail] ADD [AIInsightSummaryEnabled] [bit] NOT NULL DEFAULT 0;
 
 ALTER TABLE [BOLDBI_AI_SESSIONS] ADD [RequestType] NVARCHAR(MAX),[SessionName] NVARCHAR(MAX),[IsActive] [bit] NOT NULL DEFAULT (0),[HistoryContent] NVARCHAR(MAX),[SessionModifiedTime] DATETIMEOFFSET;
 
